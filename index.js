ab95b807cb78743789ed4535ee91e92f7329a02d
Ruben Salazar <rubensalazar@rocketmail.com>
2024-06-30 14:58:41 -0400

    sending data with fetch lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your code here
+function submitData (name, email){
+    const formData = {
+        name: "name",
+        email: "email",
+      };
+    
+    return fetch("http://localhost:3000/users", {
+        method: "Post",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+         },
+         body: JSON.stringify({name, email}),
+})
+.then(function (response) {
+    return response.json();
+  })
+  .then(function (object) {
+    console.log(object.id);
+ document.body.append(object.id)
+})
+.catch(function (error) {
+    alert("Bad things!");
+    console.log(error.message);
+document.body.append(error.message)
+ })
+}
+
+
+
+
+
